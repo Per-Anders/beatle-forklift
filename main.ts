0ed205ab6a52fc724aff259a172724730503400b
Per-Anders Bjune <45122937+Per-Anders@users.noreply.github.com>
2023-03-26 15:38:46 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        if (claw_open == false) {
-            pins.servoWritePin(AnalogPin.P2, 144)
-            claw_open = true
-        } else {
-            pins.servoWritePin(AnalogPin.P2, 180)
-            claw_open = false
-        }
+        pins.servoWritePin(AnalogPin.P2, 144)
+    }
+    if (receivedNumber == 2) {
+        pins.servoWritePin(AnalogPin.P2, 180)
     }
 })
-let claw_open = false
 DFRobotMaqueenPlus.I2CInit()
-claw_open = false
 basic.forever(function () {
     radio.setGroup(1)
 })
